7dad32fc8ccc7f2ad2373bf8e1d5769fca1b911a
KOTA TARUN VENKAT SAI KUMAR <tarunvenkatsaikumar@gmail.com>
2025-04-03 18:46:32 +0530

    add the files for the code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull tardevprep/aws_ci_py_demo
+
+# Run the Docker image as a container
+docker run -t -p 5000:5000 tardevprep/aws_ci_py_demo
