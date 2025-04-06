0d6c3e48e0921c385546fd21db5ebbe9aaf0b0d8
KOTA TARUN VENKAT SAI KUMAR <tarunvenkatsaikumar@gmail.com>
2025-04-06 15:25:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull tardevprep/aws_ci_py_demo
+docker pull tardevprep/sample-python-flask-project
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 tardevprep/aws_ci_py_demo
+docker run -d -p 5000:5000 tardevprep/sample-python-flask-project
